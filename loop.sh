2c18606efa7f3d57ba9fa6a459e51d04e40bd2e6
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-07-22 12:28:04 +0000

    Automatic script
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ loop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+while true
+do
+  res=`sudo -u postgres psql hh -tAc 'select count(distinct auditid) filter (where opid is not null) as found, count(distinct auditid) filter (where opid is null) as not_found from audit_events'`
+  echo "Found|NotFound: $res"
+  echo "Processing next 100..."
+  # res=`sudo -u postgres psql hh -tAc "update audit_events set opid = 'xxx' where requesturi = 'yyy'"`
+  res=`sudo -u postgres psql hh -tAc "`cat update_by_requesturi.sql`"`
+  echo "Updated: $res"
+  if [ "$res" = "UPDATE 0" ]
+  then
+    echo "Finished."
+    exit 0
+  fi
+  sleep 1
+done
